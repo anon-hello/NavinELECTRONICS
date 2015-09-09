93a70ce03bb8605f79354b01763f4271116b6ec4
jaimehta <jai_mehta10@yahoo.com>
2015-09-09 09:28:12 +0530

    Add

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'navin',
+		wp_ws_url : 'http://navin.co.in/wp-appkit-api/navin',
+		theme : 'wpak-left-drawer-android',
+		version : '1.0.0',
+		app_title : 'Navin Electronics',
+		debug_mode : 'off',
+		auth_key : 'put your unique phrase here',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
